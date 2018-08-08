fd1cfbb93a0c221d196da073ac3453cf1ec37cfc
Jed Jiang <jed.jiang@zoom.us>
2018-08-08 21:08:08 +0800

    添加任务列表

--- frontend/js/frame.js
+++ frontend/js/frame.js
@@ -1,24 +1,36 @@
-$(function() {
+layui.config({
+	base: './js/'
+}).extend({
+	// 加载自定义模块
+	tasklist: 'tasklist'
+})
 
-	layui.use(['layer', 'table'], function() {
-		var layer = layui.layer;
-		
-		// 创建任务
-		var CREATE_TASK = 0;
-		// 结束任务
-		var END_TASK = 1;
-		// 恢复任务
-		var RECOVER_TASK = 2;
-		
-		// 根据locationURL 初始化页面
-		initPage()
+layui.use(['layer', 'tasklist'], function() {
+	var layer = layui.layer;
+	var tasklist = layui.tasklist;
+	
+	// 创建任务
+	var CREATE_TASK = 0;
+	// 结束任务
+	var END_TASK = 1;
+	// 恢复任务
+	var RECOVER_TASK = 2;
+	
+	// 任务列表
+	try{
+		tasklist.init();
+	}catch(e){
+		console.error('task list init error.')
+	}
+	
+	initPage()
 
-		$('#startPlace').on('change',function(){
-			console.warn(this.value)
-		})
-		$('#endPlace').on('change',function(){
-			console.warn(this.value)
-		})
+	$('#startPlace').on('change',function(){
+		console.warn(this.value)
+	})
+	$('#endPlace').on('change',function(){
+		console.warn(this.value)
+	})
 //		// 开始任务
 //		$("#open_task").click(function() {
 //			COMJS.confirm('您确认要结束任务吗? 结束前请确保有任务在执行', function() {
@@ -38,133 +50,130 @@ $(function() {
 //			})
 //		})
 
-		// flag : 0-create; 1-recover; 2-end;
-		var sendCommand = function(flag, msg) {
-			var cmd = '';
-			if(flag === CREATE_TASK) {
-				cmd = "/cmd/task/create/" + msg
-			} else if(flag === END_TASK) {
-				cmd = "/cmd/task/end"
-			} else if(flag === RECOVER_TASK){
-				cmd = "/cmd/task/recover"
-			}
-			
-			if(!cmd){
-				COMJS.error("没有执行任务");
-				return;
-			}
-			$.ajax({
-					method: "GET",
-					url: COMJS.CTX_PATH + cmd
-				})
-				.done(function(resp) {
-					if(resp.code == 1) {
-						COMJS.error(resp.msg);
-					} else {
-						COMJS.success(resp.msg);
-					}
-				});
+	// flag : 0-create; 1-recover; 2-end;
+	var sendCommand = function(flag, msg) {
+		var cmd = '';
+		if(flag === CREATE_TASK) {
+			cmd = "/cmd/task/create/" + msg
+		} else if(flag === END_TASK) {
+			cmd = "/cmd/task/end"
+		} else if(flag === RECOVER_TASK){
+			cmd = "/cmd/task/recover"
 		}
-
-		/**
-		 * 实时数据流
-		 */
-		function createWebsocketUrl() {
-			var loc = window.location;
-			var uri = "ws://" + loc.host;
-			return uri.slice(0, uri.lastIndexOf(":")) + ":9321"
+		
+		if(!cmd){
+			COMJS.error("没有执行任务");
+			return;
 		}
+		$.ajax({
+				method: "GET",
+				url: COMJS.CTX_PATH + cmd
+			})
+			.done(function(resp) {
+				if(resp.code == 1) {
+					COMJS.error(resp.msg);
+				} else {
+					COMJS.success(resp.msg);
+				}
+			});
+	}
 
-		function connect() {
-			var ws_uri = createWebsocketUrl()
-			var ws = new WebSocket(ws_uri);
-			ws.onopen = function() {
-				console.log('WebSocket is connected')
-			};
+	/**
+	 * 实时数据流
+	 */
+	function createWebsocketUrl() {
+		var loc = window.location;
+		var uri = "ws://" + loc.host;
+		return uri.slice(0, uri.lastIndexOf(":")) + ":9321"
+	}
+
+	function connect() {
+		var ws_uri = createWebsocketUrl()
+		var ws = new WebSocket(ws_uri);
+		ws.onopen = function() {
+			console.log('WebSocket is connected')
+		};
+		
+		// 实时更新车子的任务状态
+		ws.onmessage = function(e) {
+			// console.log('Message:', e.data);
+			//
+			var agvStatus = JSON.parse(e.data)
+			// 当前正在执行任务的名称
+			var taskName = agvStatus['taskName']
+			// 任务是否结束
+			var isFinished = agvStatus['isFinished']
+			// 车子剩余电量
+			var battery = agvStatus['battery']
+			// x 坐标
+			var x = agvStatus['x']
+			// y 坐标
+			var y = agvStatus['y']
+			// 如果有错误, 车子的报错日志
+			var error = agvStatus['error']
 			
-			// 实时更新车子的任务状态
-			ws.onmessage = function(e) {
-				// console.log('Message:', e.data);
-				//
-				var agvStatus = JSON.parse(e.data)
-				// 当前正在执行任务的名称
-				var taskName = agvStatus['taskName']
-				// 任务是否结束
-				var isFinished = agvStatus['isFinished']
-				// 车子剩余电量
-				var battery = agvStatus['battery']
-				// x 坐标
-				var x = agvStatus['x']
-				// y 坐标
-				var y = agvStatus['y']
-				// 如果有错误, 车子的报错日志
-				var error = agvStatus['error']
-				
-				
+			
 //				$("#current_task").text(taskName)
 //				$("#current_status").text(isFinished ? '任务已结束' : '任务进行中')
 //				$("#current_battery").text(battery + '%')
 
 
-			};
-			ws.onclose = function(e) {
-				console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
-				setTimeout(function() {
-					connect();
-				}, 30000);
-			};
-			ws.onerror = function(err) {
-				console.error('Socket encountered error: ', err.message, 'Closing socket');
-				ws.close();
-			};
-		}
-		
-		// 初始化 WebSocket 连接
-		// connect()
+		};
+		ws.onclose = function(e) {
+			console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
+			setTimeout(function() {
+				connect();
+			}, 30000);
+		};
+		ws.onerror = function(err) {
+			console.error('Socket encountered error: ', err.message, 'Closing socket');
+			ws.close();
+		};
+	}
+	
+	// 初始化 WebSocket 连接
+	// connect()
 
-		// 页面初始化
-		function initPage() {
+	// 页面初始化
+	function initPage() {
 
-        // 初始化echarts实例
-        var myChart = echarts.init(document.getElementById('mainChart'));
-        // 使用刚指定的配置项和数据显示图表。
-        myChart.setOption(chartOptions(0,0,0));
-				
-				function getAngle(x1, y1,  x2, y2){
-					var x = Math.abs(x1 - x2);
-					var y = Math.abs(y1 - y2);
-					var z = Math.sqrt(x*x + y*y);
-					return  Math.round((Math.asin(y / z) / Math.PI*180));
-			 }
+		// 初始化echarts实例
+		var myChart = echarts.init(document.getElementById('mainChart'));
+		// 使用刚指定的配置项和数据显示图表。
+		myChart.setOption(chartOptions(0, 0, 0));
 
-				var target_start = [20, 40]
-				var target_end = [10, 40] 
-				function mockAnimate(){
-					var currentLocation = mockAnimate.location || [0,0]
-					var x = currentLocation[0]+1
-					var y = currentLocation[1]+1
-					if(x>target_start[0]){
-						x=target_start[0]
-					}
-					if(y>target_start[1]){
-						y=target_start[1]
-					}
-					
-				
-					if(x<target_start[0]||y<target_start[1]){
-						myChart.setOption(chartOptions(x,y,0));
-						mockAnimate.location=[x,y]
-						setTimeout(mockAnimate,800)
-					}
-					myChart.setOption(chartOptions(x,y,0));
-
-				}
-				mockAnimate()
+		function getAngle(x1, y1, x2, y2) {
+			var x = Math.abs(x1 - x2);
+			var y = Math.abs(y1 - y2);
+			var z = Math.sqrt(x * x + y * y);
+			return Math.round((Math.asin(y / z) / Math.PI * 180));
 		}
 
+		var target_start = [20, 40]
+		var target_end = [10, 40]
+
+		function mockAnimate() {
+			var currentLocation = mockAnimate.location || [0, 0]
+			var x = currentLocation[0] + 1
+			var y = currentLocation[1] + 1
+			if(x > target_start[0]) {
+				x = target_start[0]
+			}
+			if(y > target_start[1]) {
+				y = target_start[1]
+			}
+
+			if(x < target_start[0] || y < target_start[1]) {
+				myChart.setOption(chartOptions(x, y, 0));
+				mockAnimate.location = [x, y]
+				setTimeout(mockAnimate, 800)
+			}
+			myChart.setOption(chartOptions(x, y, 0));
+
+		}
+		mockAnimate()
+	}
 
 
 
-	});
-
-})
+});
---
+++ frontend/js/tasklist.js
@@ -0,0 +1,84 @@
+layui.define(['jquery', 'table'], function(exports) {
+
+	var $ = layui.$;
+	var layer = layui.layer;
+	var table = layui.table;
+
+	var me = {
+		init: function() {
+			table.render({
+				elem: '#tasklist',
+				height: '500',
+				url: COMJS.CTX_PATH + "/task/list",
+				cols: [
+					[{
+							field: 'name',
+							title: '任务',
+							width: 80
+						},
+						{
+							field: 'status',
+							title: '状态',
+							width: 80,
+							templet: function(d) {
+								var text = '待办';
+								if(d.status == 1) {
+									text = '进行中';
+								} else if(d.status == 2) {
+									text = '<span style="color: #c00;">已完成</span>';
+								}
+								return text;
+							}
+						},
+						{
+							title: '操作',
+							align: 'center',
+							toolbar: '#taskbar'
+						}
+					]
+				]
+			});
+
+			//监听工具条
+			table.on('tool(tasklist)', function(obj) {
+				var data = obj.data; //获得当前行数据
+				var layEvent = obj.event; //获得 lay-event 对应的值（也可以是表头的 event 参数对应的值）
+
+				if(layEvent === 'del') { // 删除任务
+					layer.confirm('确定删除该任务(' + data.name + ')吗？', function(index) {
+						layer.close(index);
+						me.sendTaskAction("delete", data.id);
+					});
+				}
+			});
+			
+			// 10s 刷新一次任务列表
+			setInterval(function() {
+				try {
+					table.reload('tasklist')
+				} catch(e) {
+					console.error('refresh failed.');
+				}
+			}, 10000);
+
+		},
+
+		sendTaskAction: function(action, taskId) {
+			$.ajax({
+					method: "GET",
+					url: COMJS.CTX_PATH + "/task/" + action + "/" + taskId
+				})
+				.done(function(resp) {
+					if(resp.code == 1) {
+						console.error(resp.msg);
+					} else {
+						table.reload('tasklist');
+					}
+				});
+		}
+
+	}
+
+	exports('tasklist', me)
+
+})
