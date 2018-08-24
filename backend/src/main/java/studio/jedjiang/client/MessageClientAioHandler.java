95f7c4d09bee09d44682b12f952d1b191ae9eac7
Jed <jed.jiang@zoom.us>
2018-08-25 01:36:56 +0800

    修复一些小bug：
    1.优化锁机制 trylock()代替syncornize(this)
    2.界面移除6号位
    3.充电任务完成后的的待办任务拼接不正确

--- backend/src/main/java/studio/jedjiang/client/MessageClientAioHandler.java
+++ backend/src/main/java/studio/jedjiang/client/MessageClientAioHandler.java
@@ -2,6 +2,9 @@ package studio.jedjiang.client;
 
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.log.Log;
@@ -16,6 +19,7 @@ import org.tio.server.ServerGroupContext;
 import org.tio.utils.json.Json;
 import org.tio.websocket.common.WsResponse;
 
+import cn.hutool.core.thread.ThreadUtil;
 import studio.jedjiang.bean.AGVStatus;
 import studio.jedjiang.bean.Task;
 import studio.jedjiang.service.TaskService;
@@ -25,6 +29,8 @@ public class MessageClientAioHandler implements ClientAioHandler {
 
 	private static final Log log = Logs.get();
 	private static MessagePacket heartbeatPacket = new MessagePacket();
+	
+	Lock lock = new ReentrantLock();
 
 	// 配置websocket服务
 	private ServerGroupContext wsGroupCtx;
@@ -117,70 +123,21 @@ public class MessageClientAioHandler implements ClientAioHandler {
 			// 处理结束的任务
 			if (taskStatus.isFinished()) {
 				
-				synchronized (this) {
-					
-					log.debugf("任务完成：%s" , agvResponse);
-					
-					// 获取执行中的任务
-					Task task = taskService.getOngoingTask();
-					if (task != null) {
-						// 如果数据库里执行中的任务和上报完成的任务是同一个任务，则设置状态为完成
-						if(taskStatus.getTaskName().contains(task.getName())){
-							
-							// 设置完成状态前，删除所有之前已完成的任务
-							taskService.clearFinished();
-							task.setStatus(Task.TASK_FINISHED);
-							taskService.update(task);
-							
-							// 电量小于30%需要充电
-							if(taskStatus.getBattery() < AGVClient.BATTERY_LOWER_MIN_VAL){
-								
-								// 1，新增充电任务
-								String chargeTask = AGVClient.getFromSiteToChargeSite(task.getName());
-								taskService.addByStatus(chargeTask, Task.TASK_IN_PROCESS);
-								
-								// 2，发送新增的充电任务
-								messageClient.send(chargeTask);
-								
-								// 3，如果发现有下个任务，变更下个任务： 充电站→目标站点
-								Task nextTask = taskService.findNext();
-								if(nextTask != null){
-									// 任务变更： 充电站→目标站点
-									String nextTaskName = nextTask.getName();
-									nextTask.setName(nextTaskName.substring(0,1) + "71" + nextTaskName.substring(2));
-									taskService.update(nextTask);
-									
-								}
-								
-								// \\\\\充电情况处理完毕，返回
-								return;
-							}
-							
-							
-							// 然后取下一条待办任务
-							Task nextTask = taskService.findNext();
-							if (nextTask != null) {
-								// 如果待办任务存在, 则发送任务
-								messageClient.send(nextTask.getName());
-								nextTask.setStatus(Task.TASK_IN_PROCESS);
-								taskService.update(nextTask);
-							} else {
-								// 如果没有可执行的任务，回待命区
-								// 条件：判断任务是否在待命区
-								autoReturnBack(task.getName());
-								
-							}
-							
-						}
-
-					} else {
-						// 任务清理后，自动回待命区，根据最后一次上报的任务拿到任务
-						autoReturnBack(taskStatus.getTaskName());
+				// 线程控制 ： 如果当前线程拿不到锁，直接跳过tryLock代码块
+				// synchronized (this) {}
+				if(lock.tryLock()) {
+					try {
+						log.debugf("任务完成：%s" , agvResponse);
+						handleFinished(taskStatus);
 						
+						// TODO: 发送完任务后，增加1-2秒延迟为了接受到服务器最新报文
+						ThreadUtil.safeSleep(TimeUnit.SECONDS.toMillis(2));
+						
+					} finally {
+						lock.unlock();
 					}
-
+					
 				}
-
 			}
 
 			// 不论任务是否完成，都会推送AGV状态给浏览器
@@ -188,6 +145,65 @@ public class MessageClientAioHandler implements ClientAioHandler {
 		}
 		return;
 	}
+	
+	private void handleFinished (AGVStatus taskStatus) throws Exception {
+		// 获取执行中的任务
+		Task task = taskService.getOngoingTask();
+		if (task != null) {
+			// 如果数据库里执行中的任务和上报完成的任务是同一个任务，则设置状态为完成
+			if(taskStatus.getTaskName().contains(task.getName())){
+				
+				// 设置完成状态前，删除所有之前已完成的任务
+				taskService.clearFinished();
+				task.setStatus(Task.TASK_FINISHED);
+				taskService.update(task);
+				
+				// 电量小于30%需要充电
+				if(taskStatus.getBattery() < AGVClient.BATTERY_LOWER_MIN_VAL){
+					
+					// 1，新增充电任务
+					String chargeTask = AGVClient.getFromSiteToChargeSite(task.getName());
+					taskService.addByStatus(chargeTask, Task.TASK_IN_PROCESS);
+					
+					// 2，发送新增的充电任务
+					messageClient.send(chargeTask);
+					
+					// 3，如果发现有下个任务，变更下个任务： 充电站→目标站点
+					Task nextTask = taskService.findNext();
+					if(nextTask != null){
+						// 任务变更： 充电站→目标站点
+						String nextTaskName = nextTask.getName();
+						nextTask.setName(nextTaskName.substring(0,1) + "71" + nextTaskName.substring(3));
+						taskService.update(nextTask);
+						
+					}
+					
+					// \\\\\充电情况处理完毕，返回
+					return;
+				}
+				
+				
+				// 然后取下一条待办任务
+				Task nextTask = taskService.findNext();
+				if (nextTask != null) {
+					// 如果待办任务存在, 则发送任务
+					messageClient.send(nextTask.getName());
+					nextTask.setStatus(Task.TASK_IN_PROCESS);
+					taskService.update(nextTask);
+				} else {
+					// 如果没有可执行的任务，回待命区
+					// 条件：判断任务是否在待命区
+					autoReturnBack(task.getName());
+				}
+			}
+
+		} else {
+			// 任务清理后，自动回待命区，根据最后一次上报的任务拿到任务
+			autoReturnBack(taskStatus.getTaskName());
+		}
+	}
+	
+	
 	
 	// 没有任务自动回待命区
 	public void autoReturnBack(String taskName) {
